518dca40db841f7810063fbd85f51728be53a8a1
junior brandao <edilson_brandaojunior@hotmail.com>
2023-06-03 12:31:52 -0300

    updated application.yaml server. added jwt parameters

--- authuser/src/main/java/com/ead/authuser/Repositories/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/Repositories/UserRepository.java
@@ -1,13 +1,20 @@
 package com.ead.authuser.Repositories;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
 
     boolean existsByUserName(String userName);
     boolean existsByEmail(String email);
+
+    /*como o model de user a coleção de roles está anotada como fetch tipo lazy, ultilizo
+    * essa anotação (EntityGraph, para trazer a coleção de roles junto*/
+    @EntityGraph(attributePaths = "roles" , type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findByUserName(String username);
 }
--- authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -1,19 +1,25 @@
 package com.ead.authuser.configs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity// desligando as configurações dafault do spring security
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig  {
+
+    @Autowired
+    private AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     private static  final String[]  AUTH_WHITLIST ={
         "/auth/**"
@@ -23,28 +29,34 @@ public class WebSecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
             .httpBasic()
+            .authenticationEntryPoint(authenticationEntryPoint)
             .and()
             .authorizeRequests()
             .requestMatchers(AUTH_WHITLIST).permitAll()
+            .requestMatchers(HttpMethod.GET,"/users/**").hasRole("INSTRUCTOR")
             .anyRequest().authenticated()
             .and()
-            .csrf().disable()
-            .formLogin();
+            .csrf().disable();
+           // .formLogin();
     return http.build();
   }
-
+/*
   @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-    UserDetails user = User.withUsername("admin")
-            .password(passwordEncoder().encode("1224"))
-            .roles("ADMIN")
-            .build();
-    return new InMemoryUserDetailsManager(user);
-  }
+    public UserDetailsService userDetailsService(AuthenticationManagerBuilder aut) throws Exception {
+        aut
+           .userDetailsService(userServiceImpl)
+           .passwordEncoder(passwordEncoder());
+        return aut.getDefaultUserDetailsService();
+
+    }
+
+ */
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+
 
 }

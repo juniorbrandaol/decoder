d95af91bf2925b66c93d0744df72390c215ba009
junior brandao <edilson_brandaojunior@hotmail.com>
2023-06-12 11:44:31 -0300

    fix profile prod authuser

--- config-server/src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
+++ config-server/src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 

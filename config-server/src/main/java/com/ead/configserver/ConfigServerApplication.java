78f7ff86ff07ad4454aadae4deb4518d4ba8167d
junior brandao <edilson_brandaojunior@hotmail.com>
2023-06-12 13:10:07 -0300

    fix profiles

--- config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
@@ -1,11 +1,9 @@
 package com.ead.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableAutoConfiguration
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {

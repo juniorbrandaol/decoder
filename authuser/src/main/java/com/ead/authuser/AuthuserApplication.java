385fc27c56a1beddc968c6286047d3e214846398
junior brandao <edilson_brandaojunior@hotmail.com>
2023-06-15 11:25:09 -0300

    fix

--- authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
